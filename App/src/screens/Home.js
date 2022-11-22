cf5e6dbdce99db4d207ef888658cb8b1f4326cd3
kFvrk <freefasdeconta@gmail.com>
2022-11-22 16:11:24 -0300

    App

---
+++ App/src/screens/Home.js
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Context } from '../context/authContext'
+import CustomButton from '../components/CustomButton';
+
+const Home = ({ navigation }) => {
+
+  const { state, dispatch } = useContext(Context);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Olá, {state.name}</Text>
+      <CustomButton text="Companies" onPress={() => navigation.navigate("Companies")} />
+      <CustomButton text="Users" onPress={() => navigation.navigate("Users")} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent:'space-around'
+    
+  },
+  text: {
+    fontSize: 30,
+    margin: 40
+  }
+})
+
+export default Home;
